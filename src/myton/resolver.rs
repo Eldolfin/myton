9f71936b6dbcc5ed22ac15e152a9cb12a2a125c1
oscar <90446228+Eldolfin@users.noreply.github.com>
2023-01-09 12:02:21 +0100

    update
    Signed-off-by: oscar <90446228+Eldolfin@users.noreply.github.com>

--- src/myton/resolver.rs
+++ src/myton/resolver.rs
@@ -19,35 +19,39 @@ enum Resolvable {
     While(WhileStatement),
 }
 
+type ResolveResult = Result<(), String>;
+
 impl ResolveTrait for Resolver {
-    fn resolve(&mut self, stmt: Resolvable) -> Result<(), String> {
+    fn resolve(&mut self, stmt: Resolvable) -> ResolveResult {
         match stmt {
-            Resolvable::Block(stmt) => self.resolve_block(stmt)?,
-            Resolvable::Expression(stmt) => self.resolve_expression(stmt),
-            Resolvable::Function(stmt) => self.resolve_function(stmt),
-            Resolvable::If(stmt) => self.resolve_if(stmt),
-            Resolvable::Print(stmt) => self.resolve_print(stmt),
-            Resolvable::Return(stmt) => self.resolve_return(stmt),
-            Resolvable::Var(stmt) => self.resolve_var(stmt),
-            Resolvable::While(stmt) => self.resolve_while(stmt),
+            Resolvable::Block(stmt) => self.resolve_block(&stmt),
+            Resolvable::Expression(stmt) => self.resolve_expression(&stmt),
+            Resolvable::Function(stmt) => self.resolve_function(&stmt),
+            Resolvable::If(stmt) => self.resolve_if(&stmt),
+            Resolvable::Print(stmt) => self.resolve_print(&stmt),
+            Resolvable::Return(stmt) => self.resolve_return(&stmt),
+            Resolvable::Var(stmt) => self.resolve_var(&stmt),
+            Resolvable::While(stmt) => self.resolve_while(&stmt),
         }
     }
 }
 
 impl Resolver {
-    pub fn resolve_block(&mut self, block: &BlockStatement) {
+    pub fn resolve_block(&mut self, block: &BlockStatement) -> ResolveResult {
         self.begin_scope();
         self.resolve_stmts(&block.statements);
         self.end_scope();
+        Ok(())
     }
 
-    fn resolve_stmts(&mut self, stmts: &[StmtTrait]) {
+    fn resolve_stmts(&mut self, stmts: Vec<STMT>) -> ResolveResult {
         for stmt in stmts {
-            self.resolve_stmt(stmt);
+            self.resolve_stmt(stmt)?;
         }
+        Ok(())
     }
 
-    fn resolve_stmt(&mut self, stmt: &StmtTrait) {
+    fn resolve_stmt(&mut self, stmt: &STMT) -> ResolveResult {
         match stmt {
             StmtTrait::Block(block) => self.block(block),
         }
